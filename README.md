e7e1e3663cdd2fda961b568a39fdf1f09d5090a0
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-10-08 16:33:22 -0400

    more test

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 
 ## Notes
-Expect() is the expected result \
-matcher are like toBe() \
+Expect() is the expected result\
+matcher are like toBe()
 
 To run the test `npm test`
 
 
-// CHECK FOR TRUTHY & FALSY VALUES
-// toBeNull matches only null
-// toBeUndefined matches only undefined
-// toBeDefined is the opposite of toBeUndefined
-// toBeTruthy matches anything that an if statement treats as true
-// toBeFalsy matches anything that an if statement treats as false
+CHECK FOR TRUTHY & FALSY VALUES \
+toBeNull matches only null\
+toBeDefined is the opposite of toBeUndefined\
+toBeTruthy matches anything that an if statement treats as true\
+toBeFalsy matches anything that an if statement treats as false
+toBe is primary type
+toEqual is compare object reference
--- function.test.js
+++ function.test.js
@@ -7,3 +7,35 @@ test('Adds 2 + 2 to equal 4', ()=> {
 test('Adds 2 + 2 to NOT equal 5', ()=> {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+test('Should be null', ()=> {
+  expect(functions.isNull()).toBeNull();
+});
+
+test('Should be falsy', ()=> {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test('Should be truthy', ()=> {
+  expect(functions.checkValue(2)).toBeTruthy();
+});
+
+test('User should be John Doe object', ()=> {
+  expect(functions.createUser()).toEqual({firstName: 'John', lastNmae: 'Doe'});
+});
+
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+})
+
+test('Should be under or equal 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+})
+
+test('There is no I in team', () => {
+  expect('team').to
+})
